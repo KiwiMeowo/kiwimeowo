f6f1142b60abc65190ad18620cee14582e1175c1
Kiwi <kerischan0308@gmail.com>
2024-12-19 18:35:27 +0800

    I'm krilling myself

--- spoiler.js
+++
@@ -1,6 +0,0 @@
-var spoilers=document.getElementsByClassName("spoiler");
-  for (i = 0; i < spoilers.length; i++) {
-  spoilers[i].addEventListener("click", function() {
-    this.classList.toggle("unspoiler");
-  });
-}
