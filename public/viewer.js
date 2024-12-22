904573765d2d63b71dee28cfb74fff1107188266
KiwiMeowo <kerischan0308@gmail.com>
2024-12-22 13:17:52 +0800

    Changed art gallery format a bit that they use either 'a' tag or 'button' tag, god thats a lot of work

--- public/viewer.js
+++ public/viewer.js
@@ -3,7 +3,7 @@ var art = document.getElementsByClassName('artborder');
 	var artwork=artwindow.getElementsByTagName("img")[0];
 	var artdesc=artwindow.getElementsByTagName("p")[0];
     for (i = 0; i < art.length; i++) {
-      if(art[i].parentNode.tagName!="A"){
+      if(art[i].tagName!="A"){
     art[i].addEventListener("click", function() {
       artwork.src=this.getElementsByTagName("img")[0].src;
       artdesc.innerHTML=this.getElementsByTagName("img")[0].title;
