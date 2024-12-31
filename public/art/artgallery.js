8da3832000f6bf0fe4f5cb48f68fc965129122f7
KiwiMeowo <kerischan0308@gmail.com>
2024-12-31 23:07:10 +0800

    Improved art gallery js so that I don't have to add new page button everytime i add a new mini page

--- public/art/artgallery.js
+++ public/art/artgallery.js
@@ -1,15 +1,18 @@
 var a= document.getElementsByClassName("pagenum2")[1];
+var x = document.getElementsByClassName("row");
+for (i = 0; i < x.length; i++){
+  a.innerHTML+="<button onclick='openCity(" + (x.length-i) + ")'></button><span class='numdivider'></span>";
+  }
   var b= a.getElementsByTagName("button");
+  for (i = 0; i < b.length; i++){
+    b[i].innerText=i+1;
+    }
   b[0].style.color='var(--yellow)';
 function openCity(cityName) {
-  var x = document.getElementsByClassName("row");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  document.getElementById(cityName).style.display = "flex";  
-}
-for (i = 0; i < b.length; i++){
-b[i].innerText=i+1;
+  document.getElementById(cityName.toString()).style.display = "flex";  
 }
 for (i = 0; i < b.length; i++) {
 b[i].addEventListener("click", function() {
