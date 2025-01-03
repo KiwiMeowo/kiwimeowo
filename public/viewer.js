8e6342cefa2f84b9e42123546360e4247be23c74
KiwiMeowo <kerischan0308@gmail.com>
2025-01-03 11:13:27 +0800

    I should've made the art window thing id instead of class

--- public/viewer.js
+++ public/viewer.js
@@ -1,9 +1,9 @@
 var art = document.querySelectorAll('.artborder:not(a)');
 var createwindow=document.createElement('div');
-  createwindow.classList.add('window');
+  createwindow.id="window";
   createwindow.innerHTML='<button id="x">X</button><img src=""><div class="text"><p></p></div>';
   document.body.appendChild(createwindow);
-	var artwindow=document.getElementsByClassName('window')[0];
+	var artwindow=document.getElementById('window');
 	var artwork=artwindow.getElementsByTagName("img")[0];
 	var artdesc=artwindow.getElementsByTagName("p")[0];
     for (i = 0; i < art.length; i++) {
