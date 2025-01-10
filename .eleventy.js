3c3ea107d743d686e5c5fe33784ec41e0ef0352b
KiwiMeowo <kerischan0308@gmail.com>
2025-01-10 14:44:17 +0800

    Added blog RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 const htmlmin = require("html-minifier-terser");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function (eleventyConfig) {
 
     // This will stop the default behaviour of foo.html being turned into foo/index.html
     eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
-  
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+    eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
     eleventyConfig.addTransform("htmlmin", function (content) {
         if ((this.page.outputPath || "").endsWith(".html")) {
           let minified = htmlmin.minify(content, {
