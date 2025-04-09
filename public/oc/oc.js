bcf7033e39cd1232ec9ef6e8c2147fbffcc2e1f5
KiwiMeowo <kerischan0308@gmail.com>
2025-04-09 18:44:28 +0800

    hell yea OC playlist

---
+++ public/oc/oc.js
@@ -0,0 +1,18 @@
+function unhideRel(button,BoxName) {
+    document.getElementById(BoxName).classList.remove("pop"); 
+    button.style.display = "none"; 
+  }
+function openRel(name){
+  document.getElementById(name).classList.toggle("impressactive");
+}
+var playbuttons = document.getElementById("playbuttons").getElementsByTagName("button");
+var music = document.getElementById("playlist").getElementsByTagName("a");
+var randomMusic = music[Math.floor(Math.random() * music.length)].href;
+for (i = 0; i < playbuttons.length; i++) {
+  playbuttons[i].addEventListener("click", function() {
+    var link = document.createElement("a");
+    link.href = randomMusic;
+    link.target = "_blank";
+    link.click();
+  });
+  }
--- public/oc/rel.js
+++
@@ -1,7 +0,0 @@
-function unhideRel(button,BoxName) {
-    document.getElementById(BoxName).classList.remove("pop"); 
-    button.style.display = "none"; 
-  }
-function openRel(name){
-  document.getElementById(name).classList.toggle("impressactive");
-}
