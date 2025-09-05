98769d61694d53a86384d8b92baf92cba4422a35
KiwiMeowo <kerischan0308@gmail.com>
2025-09-05 22:48:09 +0800

    added secret page, some improvements to food text decoder

--- public/blog/assets/BotsDontLookAtIt.js
+++ public/blog/assets/BotsDontLookAtIt.js
@@ -1,22 +1,34 @@
 // For blog of 19 Apr 2025
 function normal(){
-    var e = document.querySelectorAll(".text p, .tit h1");
-    for (i=0;i<e.length;i++){
-        fo=e[i].innerText.replace(/EFF-OH-OH-DEE/g, "food");
-        die=fo.replace(/DEE-I-EE-TEE/g,"diet");
-        vegg=die.replace(/VEE-EE-GEE-GEE-I-ES/g,"veggies");
-        ric=vegg.replace(/AR-I-SEE-EE/g,"rice");
-        egg=ric.replace(/EE-GEE-GEE-PEE-EL-A-EN-TEE-ES/g,"eggplants");
-        ono=egg.replace(/OH-I-EN-I-OH-EN-ES/g,"onions");
-        et=ono.replace(/EE-A-TEE/g,"eat");
-        ing=et.replace(/-I-EN-GEE/g,"ing");
-        ate=ing.replace(/A-TEE-E/g,"ate");
-        snak=ate.replace(/ES-EN-A-SEE-KAY-ES/g,"snacks");
-        noon=snak.replace(/EL-YOU-EN-SEE-AECH/g,"lunch");
-        froot=noon.replace(/EFF-AR-YOU-I-TEE-ES/g,"fruits");
-        cook=froot.replace(/SEE-OH-OH-KAY/g,"cook");
-        restyouraunt=cook.replace(/AR-EE-A-ES-TEE-A-YOU-AR-A-EN-TEE-ES/g,"restaurants");
-        mel=restyouraunt.replace(/EM-EE-A-EL-S/g,"meals");
-        e[i].innerText=mel;
+    var food = document.querySelectorAll(".food");
+    for (i=0;i<food.length;i++){
+        food[i].innerText=food[i].innerText.replace(/\bAY\b/g, "A");
+        food[i].innerText=food[i].innerText.replace(/BEE/g, "B");
+        food[i].innerText=food[i].innerText.replace(/SEE/g, "C");
+        food[i].innerText=food[i].innerText.replace(/DEE/g, "D");
+        food[i].innerText=food[i].innerText.replace(/\bEE\b/g, "E");
+        food[i].innerText=food[i].innerText.replace(/EFF/g, "F");
+        food[i].innerText=food[i].innerText.replace(/GEE/g, "G");
+        food[i].innerText=food[i].innerText.replace(/AECH/g, "H");
+        food[i].innerText=food[i].innerText.replace(/IYE/g, "I");
+        food[i].innerText=food[i].innerText.replace(/JAY/g, "J");
+        food[i].innerText=food[i].innerText.replace(/KAY/g, "K");
+        food[i].innerText=food[i].innerText.replace(/EL/g, "L");
+        food[i].innerText=food[i].innerText.replace(/EM/g, "M");
+        food[i].innerText=food[i].innerText.replace(/EN/g, "N");
+        food[i].innerText=food[i].innerText.replace(/OH/g, "O");
+        food[i].innerText=food[i].innerText.replace(/PEE/g, "P");
+        food[i].innerText=food[i].innerText.replace(/KYU/g, "Q");
+        food[i].innerText=food[i].innerText.replace(/AR/g, "R");
+        food[i].innerText=food[i].innerText.replace(/ES/g, "S");
+        food[i].innerText=food[i].innerText.replace(/TEE/g, "T");
+        food[i].innerText=food[i].innerText.replace(/YOU/g, "U");
+        food[i].innerText=food[i].innerText.replace(/VEE/g, "V");
+        food[i].innerText=food[i].innerText.replace(/DOUBLEU/g, "W");
+        food[i].innerText=food[i].innerText.replace(/EKS/g, "X");
+        food[i].innerText=food[i].innerText.replace(/WHY/g, "Y");
+        food[i].innerText=food[i].innerText.replace(/ZEE/g, "Z");
+        food[i].innerText=food[i].innerText.replace(/-/g, "");
+        food[i].innerText=food[i].innerText.toLowerCase();
     }
 }
