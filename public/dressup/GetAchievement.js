9e2e0eed61f22c36d09736649f9facff9e17d083
KiwiMeowo <kerischan0308@gmail.com>
2025-12-02 12:04:31 +0800

    Edits + upload chrismas page

--- public/dressup/GetAchievement.js
+++ public/dressup/GetAchievement.js
@@ -5,6 +5,6 @@ function getAchievement(name){
     pop.setAttribute("id","achievement");
     pop.innerHTML=`<div id="star"></div><div id="desc"><h1>Congrats! You found ${name}!</h1><p>Check the dress up room for details</p></div>`;
     document.body.prepend(pop);
-    setTimeout(function(){pop.style="left:-510px"},5000);
+    setTimeout(function(){pop.style="left:-550px"},5000);
   }
 }
