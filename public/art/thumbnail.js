d3bc755330764df6b4548ba93e948eaf2e7266eb
KiwiMeowo <kerischan0308@gmail.com>
2025-12-07 08:53:24 +0800

    fixed copy NotAllowedError in vscode, had to use a deprecated code

--- public/art/thumbnail.js
+++ public/art/thumbnail.js
@@ -93,5 +93,5 @@ function copy() {
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+  document.execCommand('copy');
 }
