593d0f931a5a86497f14ae4ad02dc826e760e0fa
KiwiMeowo <kerischan0308@gmail.com>
2025-12-21 00:40:49 +0800

    Added word count and reading time

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 const htmlmin = require("html-minifier-terser");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const { DateTime } = require("luxon");
+const { wordCount } = require("eleventy-plugin-wordcount");
+const readingTime = require('eleventy-plugin-reading-time');
 module.exports = function (eleventyConfig) {
 
     // This will stop the default behaviour of foo.html being turned into foo/index.html
     eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
     eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(wordCount);
+    eleventyConfig.addPlugin(readingTime);
     eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
     eleventyConfig.addFilter('datestring', (dateObj) => {
       return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('d LLL yyyy');
