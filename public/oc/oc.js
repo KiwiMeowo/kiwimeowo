8849c78b4961c51de70ddd71cbef61cc66523584
KiwiMeowo <kerischan0308@gmail.com>
2026-01-09 19:48:04 +0800

    Made the flipcard thing js free

--- public/oc/oc.js
+++ public/oc/oc.js
@@ -2,9 +2,6 @@ function unhideRel(button,BoxName) {
     document.getElementById(BoxName).classList.remove("pop"); 
     button.style.display = "none"; 
   }
-function toggleRel(element){
-  document.getElementById(element).classList.toggle("flip");
-}
 var playbuttons = document.getElementById("playbuttons").getElementsByTagName("button");
 var music = document.getElementById("playlist").getElementsByTagName("a");
 var randomMusic = music[Math.floor(Math.random() * music.length)].href;
