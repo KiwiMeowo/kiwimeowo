5a99d027674cb50e3608d4741f174af431e088bc
KiwiMeowo <kerischan0308@gmail.com>
2026-09-24 00:14:24 +0800

    Changed art comment widget title

--- public/art-comment-widget.js
+++ public/art-comment-widget.js
@@ -55,7 +55,7 @@ const s_filteredWords = [ // Add words to filter by putting them in quotes and s
 
 
 // Text - Change what messages/text appear on the form and in the comments section (Mostly self explanatory)
-const s_widgetTitle = 'Be Nice!';
+const s_widgetTitle = 'Comments';
 const s_nameFieldLabel = 'Name: ';
 const s_websiteFieldLabel = 'Website (Optional): ';
 const s_textFieldLabel = 'Comment your thoughts';
